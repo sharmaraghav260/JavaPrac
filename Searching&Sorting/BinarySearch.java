e6089096695ec52ac85ff8215b299a7bb261282e
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-10-16 15:59:55 -0600

    Better

--- Searching&Sorting/BinarySearch.java
+++ Searching&Sorting/BinarySearch.java
@@ -3,16 +3,15 @@ class BinarySearch
     static int search(int[] a, int l, int r, int x)
     {
         int mid  = l + (r-l)/2;
-        while(l<=r){
+        while(l<=r)
+        {
             if(a[mid] == x)
                 return x;
 
             else if(a[mid] < x)
-                search(a, mid+1, r, x);
+                return search(a, mid+1, r, x);
             
-            else
-                search(a, l, mid-1, x);
-
+            return search(a, l, mid-1, x);
         }
         // element not found
         return -1;
