129a203db53b0b28a1513ce7c2188a1532606b0d
sharmaraghav260 <sharmaraghav260@gmail.com>
2017-10-16 15:58:09 -0600

    BinarySearch Implementation Done.

---
+++ Searching&Sorting/BinarySearch.java
@@ -0,0 +1,27 @@
+class BinarySearch 
+{
+    static int search(int[] a, int l, int r, int x)
+    {
+        int mid  = l + (r-l)/2;
+        while(l<=r){
+            if(a[mid] == x)
+                return x;
+
+            else if(a[mid] < x)
+                search(a, mid+1, r, x);
+            
+            else
+                search(a, l, mid-1, x);
+
+        }
+        // element not found
+        return -1;
+    }
+
+    public static void main(String agrs []){
+        int[] test = {1,2,3,4,5,6};
+        int l = 0;
+        int r = test.length - 1;
+        System.out.print(search(test,l,r,3));
+    }
+}
